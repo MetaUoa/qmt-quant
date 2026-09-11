02b769de814e7b77d152370387f4c3c023e987d0
MetaUoa <97014379+MetaUoa@users.noreply.github.com>
2026-09-12 00:55:27 +0800

    fix: enforce frozen data policy in C7 entrypoint

--- run_v5_c7_nested_research.py
+++ run_v5_c7_nested_research.py
@@ -4,7 +4,12 @@ import sys
 
 import run_v5_c_nested_research as c1
 
-from qmt_quant.research_policy import assert_pre_holdout_end
+from qmt_quant.research_policy import (
+    DEFAULT_RESEARCH_DATA_POLICY,
+    assert_cli_float_floor,
+    assert_cli_int_floor,
+    assert_pre_holdout_end,
+)
 from qmt_quant.research_runtime import install_v5_c_contracts
 
 
@@ -14,6 +19,28 @@ _OriginalFrozenCandidate = c1.FrozenCandidate
 
 def _assert_pre_2026_only(argv: list[str]) -> None:
     assert_pre_holdout_end(argv, context="C7 research")
+
+
+def _assert_data_policy(argv: list[str]) -> None:
+    policy = DEFAULT_RESEARCH_DATA_POLICY
+    assert_cli_float_floor(
+        argv,
+        "--min-symbol-coverage",
+        minimum=policy.min_symbol_coverage,
+        default=policy.min_symbol_coverage,
+    )
+    assert_cli_float_floor(
+        argv,
+        "--min-exposure-coverage",
+        minimum=policy.min_exposure_coverage,
+        default=policy.min_exposure_coverage,
+    )
+    assert_cli_int_floor(
+        argv,
+        "--min-symbols-per-date",
+        minimum=policy.min_symbols_per_date,
+        default=policy.min_symbols_per_date,
+    )
 
 
 def _stability_policy(*args, **kwargs):
@@ -29,7 +56,9 @@ def _c7_frozen_candidate(*args, **kwargs):
 
 
 def main() -> int:
-    _assert_pre_2026_only(sys.argv[1:])
+    argv = sys.argv[1:]
+    _assert_pre_2026_only(argv)
+    _assert_data_policy(argv)
     install_v5_c_contracts(c1)
     c1.CoreAlphaPolicy = _stability_policy
     c1.FrozenCandidate = _c7_frozen_candidate
